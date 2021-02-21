8117827e9f27ebbf458726f83e1f207fe662b8f1
Filipe Duarte <filipe_pb_duarte@hotmail.com>
2021-02-21 12:06:47 -0300

    up

---
+++ code/generate_metrics.py
@@ -0,0 +1,23 @@
+###############################################################################
+from utils import load_predictions_data, generate_metrics, save_pandas_summary
+from utils import summarize_metrics_folds, pandanize_summary
+import pickle
+
+
+if __name__ == "__main__":
+
+	print("Step 1 - Loading data")
+	predictions_dict = load_predictions_data()
+
+	print("Step 2 - Started calculation")
+	metrics_dict = generate_metrics(predictions_dict)
+	# salvar esse dict
+	with open('../metrics_dict.pkl', 'wb') as outfile:
+		pickle.dump(metrics_dict, outfile)
+		
+	summary = summarize_metrics_folds(metrics_dict)
+	print("Step 2 - Finished calculation")
+
+	print("Step 3 - Storing metrics")
+	pd_summary = pandanize_summary(summary)
+	save_pandas_summary(pd_summary)
